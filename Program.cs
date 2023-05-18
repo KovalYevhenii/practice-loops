ca6e94a371d03caeb5cb21ae69453cd47e68b3dc
KovalYevhenii <kovalyevhenii@gmail.com>
2023-05-18 11:26:24 +0200

    V.3.1

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
 
-            int n;
             string? input;
 
             while (true)
@@ -17,7 +16,7 @@ namespace ConsoleApp2
                 Console.WriteLine("\nEnter the size of table between 1 - 6");
                 input = Console.ReadLine();
 
-                if (int.TryParse(input, out n))
+                if (int.TryParse(input, out int n))
                 {
                     if (n < 1 || n > 6)
                     {
@@ -47,17 +46,18 @@ namespace ConsoleApp2
 
 
 
-                Program.NameTable(input, n);
+                // Program.NameTable(input, n);
                 Program.ChessTable(input, n);
+                Program.CrossTable(input, n);
 
 
             }
         }
         static void NameTable(string input, int n)
         {
+            int halfOfTable = (int)Math.Ceiling((n * input.Length - (double)input.Length)) / 2;
             int dimension = (n * input.Length) / 2;
-            int halfOfTable =(int)Math.Ceiling( (n * input.Length - (double)input.Length))/ 2;
-            
+
             Console.WriteLine(new string('+', n * input.Length + 1) + "+");
 
             for (int i = 0; i < dimension; i++)
@@ -66,24 +66,27 @@ namespace ConsoleApp2
                 {
                     Console.WriteLine("+" + new string(' ', halfOfTable) + input + new string(' ', (halfOfTable)) + "+");
                 }
+
                 Console.WriteLine("+" + new string(' ', n * input.Length) + "+");
             }
             Console.Write(new string('+', n * input.Length + 1) + "+");
             Console.WriteLine();
         }
         static void ChessTable(string input, int n)
-
         {
-            for (int i = 0; i <= n * input.Length / 2; i++)
+            int row = n * input.Length;
+            int column = n * input.Length / 2;
+
+            for (int i = 0; i <= column; i++)
             {
                 Console.Write("+");
-                for (int j = 1; j <= n * input.Length; j++)
+
+                for (int j = 1; j <= row; j++)
                 {
                     if ((i + j) % 2 == 0)
                     {
                         Console.Write("+");
                     }
-
                     else
                     {
                         Console.Write(" ");
@@ -91,6 +94,33 @@ namespace ConsoleApp2
                 }
                 Console.WriteLine("+");
             }
+            //Console.Write(new string('+', n * input.Length + 1) + "+");
+        }
+        static void CrossTable(string input, int n)
+        {
+            int row = n * input.Length;
+            int column = n * input.Length;
+
+            for (int i = 0; i <= column; i++)
+            {
+                Console.Write("+");
+                for (int j = 0; j <= row; j++)
+                {
+                    if (i == j || i + j == row)
+                    {
+                        Console.Write("+");
+                    }
+                   
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("+");
+
+
+            }
+            Console.Write(new string('+', n * input.Length + 2) + "+");
         }
     }
 
