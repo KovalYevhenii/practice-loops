f10cb474f4a33e9567e037af238bac77d3b4aba9
KovalYevhenii <kovalyevhenii@gmail.com>
2023-05-20 10:21:05 +0200

    V2.0

--- Program.cs
+++ Program.cs
@@ -1,14 +1,10 @@
-﻿using System.Data.Common;
-using System.Globalization;
-using System.Reflection.Metadata.Ecma335;
-
+﻿using LoopPractice;
 namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
             string? input;
 
             while (true)
@@ -16,114 +12,62 @@ namespace ConsoleApp2
                 Console.WriteLine("\nEnter the size of table between 1 - 6");
                 input = Console.ReadLine();
 
-                if (int.TryParse(input, out int n))
+                if (!(int.TryParse(input, out int n) || n < 1 || n > 6))
                 {
-                    if (n < 1 || n > 6)
-                    {
-                        Console.WriteLine("Size must be between 1 - 6");
+                    Console.WriteLine("Size must be between 1 - 6 and valid number");
+                    continue;
+                }
+
+                do
+                {
+                    Console.WriteLine("Enter some text:");
+                    input = Console.ReadLine();
+                }
+                while (string.IsNullOrEmpty(input));
+
+                Console.WriteLine(
+                    "What type of table you want to build" +
+                    " 1 - Name table, 2 - Chess table, 3 - Cross table, 4 - All tables");
+
+                string? choose = Console.ReadLine();
+
+                choose = string.IsNullOrEmpty(choose) ? "you entered an empty string" : " ";
+                Console.WriteLine(choose);
+
+                switch (choose)
+                {
+                    case "1" when input.Length < 40:
+                        Table.NameTable(input, n);
+                        break;
+
+                    case "2" when input.Length < 40:
+                        Table.ChessTable(input, n);
+                        break;
+
+                    case "3" when input.Length < 40:
+                        Table.CrossTable(input, n);
+                        break;
+
+                    case "4" when input.Length < 40:
+                        Table.NameTable(input, n);
+                        Table.ChessTable(input, n);
+                        Table.CrossTable(input, n);
+                        break;
+
+                    default:
+                        if (input.Length > 40)
+                        {
+                            Console.WriteLine("String must be smaller then 40 ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong command");
+                        }
                         continue;
-                    }
                 }
-                else
-                {
-                    continue;
-                }
-
-
-                Console.WriteLine("Enter some text:");
-                input = Console.ReadLine();
-                if (string.IsNullOrEmpty(input))
-                {
-                    continue;
-                }
-                else if (input.Length > 40)
-                {
-                    Console.WriteLine("text length must be smaller the 40 characters");
-
-                }
-
-
-
-
-
-                // Program.NameTable(input, n);
-                Program.ChessTable(input, n);
-                Program.CrossTable(input, n);
-
-
             }
-        }
-        static void NameTable(string input, int n)
-        {
-            int halfOfTable = (int)Math.Ceiling((n * input.Length - (double)input.Length)) / 2;
-            int dimension = (n * input.Length) / 2;
-
-            Console.WriteLine(new string('+', n * input.Length + 1) + "+");
-
-            for (int i = 0; i < dimension; i++)
-            {
-                if (i == dimension / 2)
-                {
-                    Console.WriteLine("+" + new string(' ', halfOfTable) + input + new string(' ', (halfOfTable)) + "+");
-                }
-
-                Console.WriteLine("+" + new string(' ', n * input.Length) + "+");
-            }
-            Console.Write(new string('+', n * input.Length + 1) + "+");
-            Console.WriteLine();
-        }
-        static void ChessTable(string input, int n)
-        {
-            int row = n * input.Length;
-            int column = n * input.Length / 2;
-
-            for (int i = 0; i <= column; i++)
-            {
-                Console.Write("+");
-
-                for (int j = 1; j <= row; j++)
-                {
-                    if ((i + j) % 2 == 0)
-                    {
-                        Console.Write("+");
-                    }
-                    else
-                    {
-                        Console.Write(" ");
-                    }
-                }
-                Console.WriteLine("+");
-            }
-            //Console.Write(new string('+', n * input.Length + 1) + "+");
-        }
-        static void CrossTable(string input, int n)
-        {
-            int row = n * input.Length;
-            int column = n * input.Length;
-
-            for (int i = 0; i <= column; i++)
-            {
-                Console.Write("+");
-                for (int j = 0; j <= row; j++)
-                {
-                    if (i == j || i + j == row)
-                    {
-                        Console.Write("+");
-                    }
-                   
-                    else
-                    {
-                        Console.Write(" ");
-                    }
-                }
-                Console.WriteLine("+");
-
-
-            }
-            Console.Write(new string('+', n * input.Length + 2) + "+");
         }
     }
-
 }
 
 
---
+++ Table.cs
@@ -0,0 +1,76 @@
+﻿
+namespace LoopPractice
+{
+    static class Table
+    {
+        public static void NameTable(string input, int n)
+        {
+            int tableWidth = (n - 1) * 2 + input.Length;
+            int halfOfTable = (tableWidth - input.Length) / 2;
+            int tableLength = (n - 1) * 2;
+            int i = 0;
+
+            Console.WriteLine(new string('+', tableWidth + 1) + "+");
+            do
+            {
+                if (i == tableLength / 2)
+                {
+                    Console.WriteLine("+" + new string(' ', halfOfTable) + input + new string(' ', halfOfTable) + "+");
+                }
+
+                Console.WriteLine("+" + new string(' ', tableWidth) + "+");
+                i++;
+            }
+            while (i < tableLength);
+
+            Console.Write(new string('+', tableWidth + 1) + "+");
+            Console.WriteLine();
+        }
+        public static void ChessTable(string input, int n)
+        {
+            int row = (n - 1) * 2 + input.Length;
+            int column = n * input.Length / 2;
+
+            for (int i = 0; i <= column; i++)
+            {
+                Console.Write("+");
+
+                for (int j = 1; j <= row; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("+");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("+");
+            }
+        }
+        public static void CrossTable(string input, int n)
+        {
+            int row = (n - 1) * 2 + input.Length;
+
+            Console.WriteLine(new string('+', row + 1) + "+");
+            for (int i = 0; i < row; i++)
+            {
+                Console.Write("+");
+                for (int j = 0; j < row; j++)
+                {
+                    if (i == j || i + j == row - 1)
+                    {
+                        Console.Write("+");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("+");
+            }
+            Console.WriteLine(new string('+', row + 1) + "+");
+        }
+    }
+}
