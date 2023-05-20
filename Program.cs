1f419eda4aefdf218320f93905b4c3e2092f6412
KovalYevhenii <kovalyevhenii@gmail.com>
2023-05-20 10:56:52 +0200

    V2.1

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp2
                 Console.WriteLine("\nEnter the size of table between 1 - 6");
                 input = Console.ReadLine();
 
-                if (!(int.TryParse(input, out int n) || n < 1 || n > 6))
+                if (!(int.TryParse(input, out int n) && n >= 1 && n <= 6))
                 {
                     Console.WriteLine("Size must be between 1 - 6 and valid number");
                     continue;
@@ -30,34 +30,35 @@ namespace ConsoleApp2
                     " 1 - Name table, 2 - Chess table, 3 - Cross table, 4 - All tables");
 
                 string? choose = Console.ReadLine();
+                string message = string.IsNullOrEmpty(choose) ? "you entered an empty string" : " ";
+                Console.WriteLine(message);
 
-                choose = string.IsNullOrEmpty(choose) ? "you entered an empty string" : " ";
-                Console.WriteLine(choose);
+                var width = input.Length * n;
 
                 switch (choose)
                 {
-                    case "1" when input.Length < 40:
+                    case "1" when width < 40:
                         Table.NameTable(input, n);
                         break;
 
-                    case "2" when input.Length < 40:
+                    case "2" when width < 40:
                         Table.ChessTable(input, n);
                         break;
 
-                    case "3" when input.Length < 40:
+                    case "3" when width < 40:
                         Table.CrossTable(input, n);
                         break;
 
-                    case "4" when input.Length < 40:
+                    case "4" when width < 40:
                         Table.NameTable(input, n);
                         Table.ChessTable(input, n);
                         Table.CrossTable(input, n);
                         break;
 
                     default:
-                        if (input.Length > 40)
+                        if (width > 40)
                         {
-                            Console.WriteLine("String must be smaller then 40 ");
+                            Console.WriteLine("width must be smaller then 40 ");
                         }
                         else
                         {
